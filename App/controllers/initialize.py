d42ede087befffa78e95d3954e0eec2c15b15a3c
JOSIAHTHEPROGRAMMER <josiahalexis15@gmail.com>
2025-04-18 18:33:11 -0400

    updated models and controllers

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -1,4 +1,7 @@
 from .user import create_user
+from .ingredient import create_ingredient
+from .recipe import create_recipe
+from .recipeIngredients import add_ingredient_to_recipe
 from App.database import db
 
 
@@ -7,3 +10,15 @@ def initialize():
     db.create_all()
     create_user('bob', 'bobpass')
     create_user('joe', 'joepass')
+    create_recipe(
+    title='Spaghetti Carbonara',
+    instructions='Boil pasta. Fry pancetta. Mix eggs and cheese. Combine everything.',
+    user_id=1,
+    image='https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg'
+)
+    create_recipe(
+    title='Chicken Alfredo',
+    instructions='Cook pasta. Sauté chicken in butter. Add cream and parmesan. Combine with pasta.',
+    user_id=1,
+    image='https://www.themealdb.com/images/media/meals/syqypv1486981727.jpg'
+)
--- App/models/ingredient.py
+++ App/models/ingredient.py
@@ -4,6 +4,7 @@ from App.database import db
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    image =db.Column(db.String(100), nullable=False)
     quantity = db.Column(db.String(20))  # e.g., "2 cups", "1 tbsp"
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
