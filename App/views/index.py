771b3cf611210b8db542e2d721fcc075624e2771
Tyrese Des Vignes <34144500+Im2silent@users.noreply.github.com>
2025-04-19 23:02:42 +0000

    index update

--- App/views/index.py
+++ App/views/index.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+from flask_jwt_extended import jwt_required
 from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
