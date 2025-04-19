fbd73fae3d2f64e8f5a97735470c41481674c8da
Tyrese Des Vignes <34144500+Im2silent@users.noreply.github.com>
2025-04-19 22:55:57 +0000

    index update

--- App/views/index.py
+++ App/views/index.py
@@ -4,8 +4,19 @@ from App.controllers import create_user, initialize
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
+@jwt_required(optional=True)
 def index_page():
-    return render_template('index.html')
+    user = current_user if current_user else None
+    is_authenticated = user is not None
+    recipes = get_all_recipes()
+    recipes_json = [r.get_json() for r in recipes]
+    return render_template(
+        'index.html',
+        current_user=user,
+        is_authenticated=is_authenticated,
+        recipes=recipes,
+        recipes_json = recipes_json
+    )
 
 @index_views.route('/init', methods=['GET'])
 def init():
