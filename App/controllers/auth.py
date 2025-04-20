2f67087418dbf7099b8153d59d01a86a0b869aac
Tyrese Des Vignes <34144500+Im2silent@users.noreply.github.com>
2025-04-20 01:38:41 +0000

    revert

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -29,14 +29,13 @@ def setup_jwt(app):
 def add_auth_context(app):
   @app.context_processor
   def inject_user():
-    try:
-        verify_jwt_in_request()
-        user_id = get_jwt_identity()
-        print(f"User ID from JWT: {user_id}")
-        current_user = User.query.get(user_id)
-        is_authenticated = True
-    except Exception as e:
-        print("JWT Verification Error:", e)
-        is_authenticated = False
-        current_user = None
-    return dict(is_authenticated=is_authenticated, current_user=current_user)
+      try:
+          verify_jwt_in_request()
+          user_id = get_jwt_identity()
+          current_user = User.query.get(user_id)
+          is_authenticated = True
+      except Exception as e:
+          print(e)
+          is_authenticated = False
+          current_user = None
+      return dict(is_authenticated=is_authenticated, current_user=current_user)
