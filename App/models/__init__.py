7aa67e51ff54925d00807dc4152170c8d5e26ddc
Tyrese Des Vignes <34144500+Im2silent@users.noreply.github.com>
2025-04-20 01:59:31 +0000

    fixed changes

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,10 +1,3 @@
 from .user import *
 from .recipe import *
-<<<<<<< HEAD
-from .ingredient import *
-from .recipeIngredient import *
-
-
-=======
 from .recipeIngredient import RecipeIngredients
->>>>>>> 2f67087418dbf7099b8153d59d01a86a0b869aac
