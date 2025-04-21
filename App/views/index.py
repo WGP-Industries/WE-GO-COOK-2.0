f0c9e8bdc00e6a86164421bdf47d808c09344149
Tyrese Des Vignes <34144500+Im2silent@users.noreply.github.com>
2025-04-21 16:00:07 +0000

    update index.py

--- App/views/index.py
+++ App/views/index.py
@@ -39,3 +39,35 @@ def init():
 @index_views.route('/health', methods=['GET'])
 def health_check():
     return jsonify({'status':'healthy'})
+
+@index_views.route('/addRecipe', methods=['POST'])
+@jwt_required()
+def add_recipe():
+    title = request.form.get('title')
+    instructions = request.form.get('instructions')
+    image = request.form.get('image') or ""
+    user_id = current_user.id
+
+    page = request.args.get("page", 1, type=int)
+    q = request.args.get("q", default='', type=str)
+    sort_by = request.args.get("sort_by", default='title_asc')
+
+    if not title or not instructions:
+        flash("Missing required fields", "error")
+        return redirect(url_for('index_views.index_page', page=page, q=q, sort_by = sort_by))
+
+    recipe = create_recipe(title, instructions, user_id, image)
+
+    
+    num_ingredients = int(request.form.get('num-ingredients') or 1)
+
+    for i in range(num_ingredients):
+        name = request.form.get(f'ingredient_name_{i}') or "none"
+        quantity = request.form.get(f'ingredient_quantity_{i}') or "0"
+        unit = request.form.get(f'ingredient_unit_{i}') or "none"
+        img = request.form.get(f'ingredient_image_{i}') or "none"
+        if name and quantity:
+            add_ingredient_to_recipe(recipe.id, name, quantity, img, unit)
+    
+    flash("Recipe added", "success")
+    return redirect(url_for('index_views.index_page', page=page, q=q, sort_by = sort_by))
