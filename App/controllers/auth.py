82893b3b1af065daa5cb5b923a380c048f23d090
Tyrese Des Vignes <34144500+Im2silent@users.noreply.github.com>
2025-04-21 13:44:09 +0000

    update auth.py

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -5,27 +5,23 @@ from App.models import User
 def login(username, password):
   user = User.query.filter_by(username=username).first()
   if user and user.check_password(password):
-    return create_access_token(identity=username)
+    return create_access_token(identity=str(user.id))
   return None
 
 
 def setup_jwt(app):
-  jwt = JWTManager(app)
+    jwt = JWTManager(app)
 
-  # configure's flask jwt to resolve get_current_identity() to the corresponding user's ID
-  @jwt.user_identity_loader
-  def user_identity_lookup(identity):
-    user = User.query.filter_by(username=identity).one_or_none()
-    if user:
-        return user.id
-    return None
+    @jwt.user_identity_loader
+    def user_identity_lookup(identity):
+        return identity 
 
-  @jwt.user_lookup_loader
-  def user_lookup_callback(_jwt_header, jwt_data):
-    identity = jwt_data["sub"]
-    return User.query.get(identity)
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.get(int(identity))  
 
-  return jwt
+    return jwt
 
 
 # Context processor to make 'is_authenticated' available to all templates
