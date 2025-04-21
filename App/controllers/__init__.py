1a4a69537702a86bd1b3e62445cf68d083529f49
Josiah Alexis <157635371+JOSIAHTHEPROGRAMMER@users.noreply.github.com>
2025-04-21 11:09:17 -0400

    Update __init__.py

--- App/controllers/__init__.py
+++ App/controllers/__init__.py
@@ -1,3 +1,7 @@
 from .user import *
 from .auth import *
 from .initialize import *
+from .recipe import *
+from .ingredient import *
+from .recipeIngredient import *
+from .missing_ingredients import *
