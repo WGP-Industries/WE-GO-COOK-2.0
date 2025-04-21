63ba78d3725f9f1dea7df40c5c6ef3fad21b093a
Josiah Alexis <157635371+JOSIAHTHEPROGRAMMER@users.noreply.github.com>
2025-04-21 10:04:50 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -3,24 +3,23 @@ from App.database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(20), nullable=False, unique=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    recipes = db.relationship('Recipe', back_populates='user', cascade='all, delete-orphan')
 
+   
     def __init__(self, username, password):
         self.username = username
         self.set_password(password)
 
     def get_json(self):
-        return{
+        return {
             'id': self.id,
             'username': self.username
         }
 
     def set_password(self, password):
-        """Create hashed password."""
         self.password = generate_password_hash(password)
-    
-    def check_password(self, password):
-        """Check hashed password."""
-        return check_password_hash(self.password, password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
