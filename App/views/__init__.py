1aec8745bfd4e99dcdd0fc4b4747fc760a4f02d4
Josiah Alexis <157635371+JOSIAHTHEPROGRAMMER@users.noreply.github.com>
2025-04-21 14:11:49 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -5,6 +5,8 @@ from .index import index_views
 from .auth import auth_views
 from .admin import setup_admin
 from .ingredient import ingredient_views
+from .missing_ingredients import missing_ingredients_views
 
-views = [user_views, index_views, auth_views, ingredient_views] 
+
+views = [user_views, index_views, auth_views, ingredient_views, missing_ingredients_views] 
 # blueprints must be added to this list
